7c1d72a9e86f714c3afbbff565a495cd49a85593
Adri <adri.nemeth92@gmail.com>
2017-05-05 18:44:46 +0200

    added audio to the app
    flashing elements play voice both when player clicks and when the
    computer adds a new move

--- simon.js
+++ simon.js
@@ -58,6 +58,8 @@ var computerView = {
         setTimeout(function () {
             currentDiv = document.getElementById(arr[i]);
             blinker(currentDiv);
+            this.audio = document.getElementById(arr[i] + "-audio");
+            this.audio.play();
         }, 1000 * i);
     },
     lightUp: function (item) {
@@ -68,51 +70,51 @@ var computerView = {
     }
 };
 var playerView = {
-  init: function() {
-    this.parentDiv = document.getElementById("parent-div");
-    playerView.render();
-  },
-  render: function() {
-    var counter = 0;
-    this.parentDiv.addEventListener("click", function(e) {
-      //ignore clicks on parent div
-      if (e.target !== e.currentTarget) {
-        //run code if clicked element is not parent div
-        this.item = document.getElementById(e.target.id);
-        console.log("player: " + model.player);
-        console.log("counter: " + counter);
-        console.log("id: " + typeof e.target.id, "current move: " + typeof model.moves[counter]);
-        console.log("id: " + e.target.id, "current move: " + model.moves[counter]);
-        computerView.lightUp(this.item);
-        if (e.target.id !== model.moves[counter]) {
-          console.log("wrong");
-          setTimeout(function() {
-            computerView.init();
-          }, 1000);
-        } else {
-          controller.addPlayerMove(e.target.id);
+    init: function () {
+        this.parentDiv = document.getElementById("parent-div");
+        playerView.render();
+    },
+    render: function () {
+        var counter = 0;
+        this.parentDiv.addEventListener("click", function (e) {
+            //ignore clicks on parent div
+            if (e.target !== e.currentTarget) {
+                //run code if clicked element is not parent div
+                this.item = document.getElementById(e.target.id);
+                console.log("player: " + model.player);
+                console.log("counter: " + counter);
+                console.log("id: " + typeof e.target.id, "current move: " + typeof model.moves[counter]);
+                console.log("id: " + e.target.id, "current move: " + model.moves[counter]);
+                computerView.lightUp(this.item);
+                if (e.target.id !== model.moves[counter]) {
+                    console.log("wrong");
+                    setTimeout(function () {
+                        computerView.init();
+                    }, 1000);
+                } else {
+                    controller.addPlayerMove(e.target.id);
 
-         counter++;
-          console.log(counter);
-          if (
-            model.player.length == model.moves.length &&
-            model.player.length !== 0
-          ) {
-            controller.resetPlayer();
-            counter = 0;
+                    counter++;
+                    console.log(counter);
+                    if (
+                        model.player.length == model.moves.length &&
+                        model.player.length !== 0
+                    ) {
+                        controller.resetPlayer();
+                        counter = 0;
 
-            console.log("newRound");
-            setTimeout(function() {
-              controller.addMove();
-              controller.updateScore();
-              scoreView.render();
-              computerView.init();
-            }, 2000);
-          }
-        }
-      }
-    });
-  }
+                        console.log("newRound");
+                        setTimeout(function () {
+                            controller.addMove();
+                            controller.updateScore();
+                            scoreView.render();
+                            computerView.init();
+                        }, 2000);
+                    }
+                }
+            }
+        });
+    }
 };
 scoreView.init();
 playerView.init();
